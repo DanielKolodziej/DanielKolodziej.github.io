5807f3c10bf0f079db78e1fb5206e40b8675b118
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-07-11 09:19:47 -0500

    nav content and styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Grid, useMediaQuery } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    background: {
+      backgroundImage: 'linear-gradient( 180deg, #090909 25%, #545454 130%)',
+      padding: '1em',
+    },
+    links: {
+        color: '#fff',
+        marginRight: '2em',
+    },
+  });
 
 const Nav = () => {
-  return (
-    <div>
-      nav
-    </div>
-  );
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width: 924px)');
+    return (
+        <Grid 
+            container 
+            className={classes.background} 
+            justify="flex-end" 
+            alignItems="center">
+            <Grid item>
+                <Typography variant="a" component="a" className={classes.links}>
+                    Projects
+                </Typography> 
+            </Grid>
+            <Grid item>
+                <Typography variant="a" component="a" className={classes.links}>
+                    Skills
+                </Typography> 
+            </Grid>
+            <Grid item>
+                <Typography variant="a" component="a" className={classes.links}>
+                    About Me
+                </Typography> 
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Nav;
