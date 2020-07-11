8d01acd916d925205a4f87b8bfb2c2eceb4231e2
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-07-11 17:42:21 -0500

    styling and content to about component

--- src/About.js
+++ src/About.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Grid, useMediaQuery } from '@material-ui/core';
+import meIMG from './images/me.jpg';
+import projIMG from './images/DashMobile.PNG';
+
+const useStyles = makeStyles({
+    background: {
+        padding: '2em 1em 2em 1em',
+        height: '100vh',
+    },
+    title: {
+        backgroundColor: '#fff',
+    },
+    list: {
+        color: '#fff',
+        padding: '1em 0'
+    }
+  });
 
 const About = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width: 844px)');
   return (
-    <div>
-      about
-    </div>
+    <Grid container className={classes.background}>
+        <Grid item xs={12} sm={6}>
+            <img src={meIMG} style={{width: '90%'}}/>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+                <Typography variant="h2" component="h2" className={classes.title}>
+                    About Me.
+                </Typography> 
+                <Typography variant="body1" component="p">
+                    My passion is in designing, developing, and deploying web applications.  I would make a great addition to your team because of my willingness to embrace changing technologies, collaborate with others, and take on rewarding challenges.
+                </Typography> 
+        </Grid>
+    </Grid>
   );
 }
 
