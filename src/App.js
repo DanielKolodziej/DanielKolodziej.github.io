180147abeeb6e9eb7686e7e884af4ababd363f35
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-07-11 09:01:34 -0500

    added component sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Nav from './Nav';
+import Hero from './Hero';
+import Projects from './Projects';
+import Skills from './Skills';
+import About from './About';
+import Footer from './Footer';
 
 const App = () => {
   return (
-    <div className="App">
-      app
-    </div>
+    <>
+      <Nav />
+      <Hero />
+      <Projects />
+      <Skills />
+      <About />
+      <Footer />
+    </>
   );
 }
 
