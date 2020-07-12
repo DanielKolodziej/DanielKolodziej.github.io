b054b08a3ff42bd84dd6c704c3d0a90d347d6743
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-07-11 19:34:11 -0500

    Updates

---
+++ precache-manifest.71bfbdf55912144d83ebe29fc2ed85b1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2610cd5c7180400b493d85e9ffd48474",
+    "url": "/DanielKolodziej.github.io/index.html"
+  },
+  {
+    "revision": "399fa8e2d91948ef4b09",
+    "url": "/DanielKolodziej.github.io/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5db253a138b8ff4b52d5",
+    "url": "/DanielKolodziej.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "399fa8e2d91948ef4b09",
+    "url": "/DanielKolodziej.github.io/static/js/2.adca986c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DanielKolodziej.github.io/static/js/2.adca986c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db253a138b8ff4b52d5",
+    "url": "/DanielKolodziej.github.io/static/js/main.bab5a395.chunk.js"
+  },
+  {
+    "revision": "d2391c620b22333c59e4",
+    "url": "/DanielKolodziej.github.io/static/js/runtime-main.6b75b9af.js"
+  },
+  {
+    "revision": "f1cb97cd0094ae89cbad66e7fa687c81",
+    "url": "/DanielKolodziej.github.io/static/media/DashMobile.f1cb97cd.PNG"
+  },
+  {
+    "revision": "fe4b48cb126695f6f92992f3abe965a7",
+    "url": "/DanielKolodziej.github.io/static/media/databaseIT.fe4b48cb.PNG"
+  },
+  {
+    "revision": "f07360b46497f9bfabda7a8381da0054",
+    "url": "/DanielKolodziej.github.io/static/media/jobApplications.f07360b4.PNG"
+  },
+  {
+    "revision": "55bf1f20132a200ee9b75c9e1ea52a95",
+    "url": "/DanielKolodziej.github.io/static/media/me.55bf1f20.jpg"
+  },
+  {
+    "revision": "f7bcd15308814cf29e27675c3e2f1314",
+    "url": "/DanielKolodziej.github.io/static/media/messengerApp.f7bcd153.PNG"
+  },
+  {
+    "revision": "502d8f95e78c510c4ea20d64bcdc5cbb",
+    "url": "/DanielKolodziej.github.io/static/media/noteApp.502d8f95.PNG"
+  },
+  {
+    "revision": "746db13db1596a85d8ab2b10b26627cf",
+    "url": "/DanielKolodziej.github.io/static/media/weatherApp.746db13d.PNG"
+  }
+]);
