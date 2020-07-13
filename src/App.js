a8c22d45afdee2e1d69ad90e2658c503569c1fc8
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-07-13 18:24:55 -0500

    scrollbar styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import Skills from './Skills';
 import About from './About';
 import Footer from './Footer';
 
+// //scrollbar for mozilla
+// scrollbarWidth: 'thin',
+// scrollbarColor: 'rgba(0,0,0,0.3) #ffc324',
+// // scrollbar for chrome/edge
+// '&::-webkit-scrollbar-track': {
+//   // backgroundColor: 'rgba(0,0,0,0.5)',
+//   borderRadius: '10px',
+//   boxShadow: 'inset 0 0 6px rgba(0,0,0,0.85)',
+// },
+// '&::-webkit-scrollbar': {
+//   height: '7px',
+// },
+// '&::-webkit-scrollbar-thumb': {
+//   borderRadius: '10px',
+//   backgroundColor: '#FFF',
+//   backgroundImage: '-webkit-linear-gradient(top,#f5dc9a 0%,#fcd879 50%,#ffc324 51%,#ffcf52 100%)',
+// },
+
 const App = () => {
   const refSection1 = useRef()
   const refSection2 = useRef()
